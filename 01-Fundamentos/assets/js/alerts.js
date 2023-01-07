917c49090a2b258502ced62c034c21ad8fa48dd2
mishelrodri <rp18027@ues.edu.sv>
2023-01-06 22:34:06 -0600

    🌱 Learn use the console.log and alerts

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,6 @@
+
+alert('Hola Mundo')
+
+const respuesta = prompt('Cuál es tu nombre?', "Mishel")
+
+const respuesta2 = confirm("estas seguro que te gusta el mango?")
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,10 @@
+// console.log('Hola Mundo')
+
+let a = "Hola",
+    b = " Mishel",
+    c = a + b;
+
+// console.log(c);
+// console.table({ a, b, c })
+
+window.outerWidth = 1000;
