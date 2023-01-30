de918f857125b61f0f1b8415ad94612e34129fa2
mishelrodri <rp18027@ues.edu.sv>
2023-01-29 22:47:56 -0600

    CRUD en JS !!

--- 06-http/src/index.js
+++ 06-http/src/index.js
@@ -1,5 +1,19 @@
 // import { init } from "./js/chistes-page";
 // import { obtenerUsuarios } from "./js/http-provider";
-import { init } from "./js/usuarios-page";
+// import { init } from "./js/usuarios-page";
 
-init();
+import * as CRUD from "./js/crud-provaider";
+// init();
+
+CRUD.getUsuario(1).then(console.log);
+CRUD.crearUsuario({
+  name: "Fernando",
+  job: "Carpintero",
+}).then(console.log);
+
+CRUD.actualizarUsuario(1, {
+  name: "Mishel",
+  job: "developrer",
+}).then(console.log);
+
+CRUD.borrarUsuario(1).then(console.log);
